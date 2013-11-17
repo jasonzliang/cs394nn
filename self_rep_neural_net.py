1adbb6637b2bd577e030adfa705bdcec29991819
jasonzliang <jasonzliang@berkeley.edu>
2013-11-17 11:36:42 -0600

    optimizationg with CMAES

--- self_rep_neural_net.py
+++ self_rep_neural_net.py
@@ -5,7 +5,8 @@ Created on Nov 16, 2013
 '''
 from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, FullConnection
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.optimization import CMAES
+from pybrain.optimization import CMAES, NelderMead, ExactNES, FEM
+import csv, time, cPickle
 
 def generateNetwork(structure):
 #   n = buildNetwork(*structure, fast=False, outclass=SigmoidLayer)
@@ -44,14 +45,67 @@ def fitnessFunction(weights):
     error += abs(weight - output)
   return error
 
+def logNet():
+  f = open('results_' + str(networkStructure) + '_' + str(time.time()) + '.pkl', 'wb')
+  cPickle.dump(myNetwork, f)
+  f.close()
+  
+def loadNet(filename):
+  f = open(filename, 'rb')
+  global myNetwork
+  myNetwork = cPickle.load(f)
+  f.close()
+
+def position2input(pos):
+  myInput = [int(x) for x in list('{0:0b}'.format(pos))]
+  while len(myInput) < networkStructure[0]:
+    myInput.insert(0,0)
+    
+  return myInput
+
 def experiment1():
   l = CMAES(fitnessFunction, myNetwork.params)
   l.minimize = True
   l.verbose = True
+  l.maxLearningSteps = 500
+  params, fitness = l.learn()
+  myNetwork._setParameters(params)
+  logNet()
+
+def experiment2():
+  l = NelderMead(fitnessFunction, myNetwork.params)
+  l.minimize = True
+  l.verbose = True
+  l.maxLearningSteps = 1000
   l.learn()
+  logNet()
+  
+def experiment3():
+  l = ExactNES(fitnessFunction, myNetwork.params)
+  l.minimize = True
+  l.verbose = True
+  l.maxLearningSteps = 1000
+  l.learn()
+  logNet()
+  
+def experiment4():
+  l = FEM(fitnessFunction, myNetwork.params)
+  l.minimize = True
+  l.verbose = True
+  l.maxLearningSteps = 1000
+  l.learn()
+  logNet()
   
 if __name__ == '__main__':
   experiment1()
+#   loadNet('results_[8, 16, 1]_1384670399.89.pkl')
+#   error = 0.0
+#   for i,weight in enumerate(myNetwork.params):
+#     output = myNetwork.activate(position2input(i))[0]
+#     print weight, output
+#     error += abs(weight - output)
+#     
+#   print error
 #     n = generateNetwork([2,2,1])
 #     n.params = [1,0,0,0,0,0]
 #     print n.params
